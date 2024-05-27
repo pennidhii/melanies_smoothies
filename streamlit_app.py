36d6ea3200721741ef46af283e82e64ea7d74ba4
pennidhii <vpennidhi@gmail.com>
2024-05-27 15:53:47 +0530

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,6 @@ st.write(
 
 import streamlit as st
 
-cnx=st.connection("snowflake")
-session=cnx.session()
-
 name_on_order=st.text_input('Name on Smoothie')
 st.write('The name on your Smoothie will be',name_on_order)
 
